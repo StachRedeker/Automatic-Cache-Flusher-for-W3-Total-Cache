d50ee7ea4c2e85aeb36dacb35c69816c52e73465
Stach Redeker <64889011+StachRedeker@users.noreply.github.com>
2023-03-12 21:27:28 +0100

    Update cacheFlusher.php

--- cacheFlusher.php
+++ cacheFlusher.php
@@ -5,6 +5,8 @@ Plugin URI: https://github.com/StachRedeker/Automatic-Cache-Flusher-for-W3-Total
 Description: This plugin flushes the W3 Total Cache after a plugin update.
 Version: 1.0
 Author: <a href="https://www.stachredeker.nl/">Stach Redeker</a> & <a href="https://colingerritsen.com/">Colin Gerritsen</a>
+License: GPL v3 or later
+License URI: https://gnu.org/licenses/gpl-3.0.html
 */
 
 function w3tc_flush_on_plugin_update( $upgrader_object, $options ) {
@@ -16,3 +18,18 @@ function w3tc_flush_on_plugin_update( $upgrader_object, $options ) {
     }
 }
 add_action( 'upgrader_process_complete', 'w3tc_flush_on_plugin_update', 10, 2 );
+
+/*
+Automatic Cache Flusher for W3 Total Cache is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+Automatic Cache Flusher for W3 Total Cache is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Automatic Cache Flusher for W3 Total Cache. If not, see https://gnu.org/licenses/gpl-3.0.html.
+*/
